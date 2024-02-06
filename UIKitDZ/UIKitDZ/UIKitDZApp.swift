1b3fb538e7c463ecc33e7480b8046144d6c7921a
Ruslan <ruslan99992222@gmail.com>
2024-02-06 09:26:54 +0300

    init

---
+++ UIKitDZ/UIKitDZ/UIKitDZApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitDZApp.swift
+//  UIKitDZ
+//
+//  Created by Руслан Абрамов on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct UIKitDZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
