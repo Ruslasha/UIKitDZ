c445801b32ce1273d0b80f9da221cd5d82b76ff2
Ruslan <ruslan99992222@gmail.com>
2024-02-08 11:37:35 +0300

    Сделал все основное

--- UIKitDZ/UIKitDZ/AppDelegate.swift
+++ UIKitDZ/UIKitDZ/AppDelegate.swift
@@ -5,13 +5,17 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(
         _ application: UIApplication,
 
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Override point for customization after application launch.
-        true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
     }
 
     // MARK: UISceneSession Lifecycle
