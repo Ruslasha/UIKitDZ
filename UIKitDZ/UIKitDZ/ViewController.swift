b52e205debe525afd998442a2c02b15321a1caa0
Ruslan <ruslan99992222@gmail.com>
2024-02-09 08:22:30 +0300

    Задание готово

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -5,7 +5,26 @@ import UIKit
 
 /// ViewController
 class ViewController: UIViewController {
-//        override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
+    @IBOutlet var login: UITextField!
+    @IBOutlet var password: UITextField!
+
+    @IBOutlet var enterButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        password.isSecureTextEntry = true
+        login.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+        password.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+    }
+
+    @objc func textDidChange(_ textField: UITextField) {
+        guard let loginText = login.text,
+              let passwordText = password.text
+        else {
+            return
+        }
+        let isLoginFilled = !loginText.isEmpty
+        let isPasswordFilled = !passwordText.isEmpty
+
+        enterButton.alpha = isLoginFilled && isPasswordFilled ? 1.0 : 0.5
+    }
 }
