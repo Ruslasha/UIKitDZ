24e165bf051732b1b4e26174efb37601ae4f9c72
Ruslan <ruslan99992222@gmail.com>
2024-02-12 13:48:04 +0300

    фикс

---
+++ UIKitDZ/UIKitDZ/PlayerViewController.swift
@@ -0,0 +1,30 @@
+// PlayerViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+import UIKit
+
+/// Экран плеера
+class PlayerViewController: UIViewController {
+    // MARK: - Types
+
+    // MARK: - Constants
+
+    // MARK: - IBOutlets
+
+    // MARK: - Visual Components
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Life Cycle
+
+    // MARK: - Public Methods
+
+    // MARK: - IBAction
+
+    // MARK: - Private Methods
+}
--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -3,10 +3,50 @@
 
 import UIKit
 
-/// ViewController
+///  Стартовый экран приложения
 class ViewController: UIViewController {
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
+    // MARK: - Types
+
+    // MARK: - Constants
+
+    // MARK: - IBOutlets
+
+    @IBOutlet var thridButton: UIButton!
+    @IBOutlet var secondButton: UIButton!
+    @IBOutlet var firstButton: UIButton!
+    @IBOutlet var thridMusicImageView: UIImageView!
+    @IBOutlet var secondMusicImageView: UIImageView!
+    @IBOutlet var firstMusicImageView: UIImageView!
+
+    // MARK: - Visual Components
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Life Cycle
+
+//        override func viewDidLoad() {
+//            super.viewDidLoad()
+//        }
+
+    // MARK: - Public Methods
+
+    // MARK: - IBAction
+
+    @IBAction func tapFirstButton(_ sender: Any) {
+        performSegue(withIdentifier: "showPlayerViewController", sender: nil)
+    }
+
+    @IBAction func tapSecondButton(_ sender: Any) {
+        performSegue(withIdentifier: "showPlayerViewController", sender: nil)
+    }
+
+    @IBAction func tapThridButton(_ sender: Any) {
+        performSegue(withIdentifier: "showPlayerViewController", sender: nil)
+    }
+
+    // MARK: - Private Methods
 }
