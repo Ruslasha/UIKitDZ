66add0dad14c988786b6bc2a610f173b73397825
Ruslan <ruslan99992222@gmail.com>
2024-02-12 21:58:16 +0300

    Исправления

--- UIKitDZ/UIKitDZ/PlayerViewController.swift
+++ UIKitDZ/UIKitDZ/PlayerViewController.swift
@@ -1,6 +1,7 @@
 // PlayerViewController.swift
 // Copyright © RoadMap. All rights reserved.
 
+import AVFoundation
 import Foundation
 import UIKit
 
@@ -12,31 +13,110 @@ class PlayerViewController: UIViewController {
 
     // MARK: - IBOutlets
 
-    @IBOutlet weak var titleMusic: UILabel!
-    @IBOutlet weak var LogoImageView: UIImageView!
+    @IBOutlet var nextTrack: UIButton!
+    @IBOutlet var previousTrack: UIButton!
+    @IBOutlet var volumeSlider: UISlider!
+    @IBOutlet var backButton: UIButton!
+    @IBOutlet var sliderMusic: UISlider!
+    @IBOutlet var playStopImageView: UIImageView!
+    @IBOutlet var titleMusic: UILabel!
+    @IBOutlet var groupMusic: UILabel!
+    @IBOutlet var logoImageView: UIImageView!
+
     // MARK: - Visual Components
 
     // MARK: - Public Properties
 
     var logoImage: UIImage?
     var music: String?
+    var group: String?
 
     // MARK: - Private Properties
+
+    private var player = AVAudioPlayer()
+    private var isMusicPlayed = true
+    private let tracks = ["Let it be", "Yesterday"]
+    private let logos = ["Image1", "Image2"]
+    private var currentTrackIndex = 0
 
     // MARK: - Initializers
 
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
-            super.viewDidLoad()
-            
-        LogoImageView.image = image
-        titleMusic.text = labelText
-        }
+        super.viewDidLoad()
+
+        let systemImage = UIImage(systemName: "xmark")
+        backButton.setImage(systemImage, for: .normal)
+
+        logoImageView.image = logoImage
+        titleMusic.text = music
+        groupMusic.text = group
+
+        setMusic(music ?? "")
+
+        player.play()
+    }
 
     // MARK: - Public Methods
 
+    func setMusic(_ music: String) {
+        do {
+            if let audioPath = Bundle.main.path(forResource: music, ofType: "mp3") {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+                sliderMusic.maximumValue = Float(player.duration)
+            }
+        } catch {
+            print("errror")
+        }
+    }
+
     // MARK: - IBAction
+
+    @IBAction func changeSlider(_ sender: UISlider) {
+        player.currentTime = TimeInterval(sender.value)
+    }
+
+    @IBAction func changeVolumeSlider(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
+
+    @IBAction func tapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func tapPlayStopButton(_ sender: Any) {
+        if isMusicPlayed == true {
+            player.stop()
+            isMusicPlayed = false
+            playStopImageView.image = UIImage(named: "play")
+        } else {
+            player.play()
+            isMusicPlayed = true
+            playStopImageView.image = UIImage(named: "stop")
+        }
+    }
+
+    @IBAction func nextTrack(_ sender: Any) {
+        currentTrackIndex += 1
+        if currentTrackIndex >= tracks.count {
+            currentTrackIndex = 0
+        }
+        let nextTrack = tracks[currentTrackIndex]
+        logoImageView.image = UIImage(named: logos[currentTrackIndex])
+        titleMusic.text = tracks[currentTrackIndex]
+        groupMusic.text = "The Beatles"
+        setMusic(nextTrack)
+    }
+
+    @IBAction func previousTrack(_ sender: Any) {
+        currentTrackIndex -= 1
+        if currentTrackIndex < 0 {
+            currentTrackIndex = tracks.count - 1
+        }
+        let previousTrack = tracks[currentTrackIndex]
+        setMusic(previousTrack)
+    }
 
     // MARK: - Private Methods
 }
--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
 
     // MARK: - IBOutlets
 
+    @IBOutlet var thridGroup: UILabel!
+    @IBOutlet var secondGroup: UILabel!
+    @IBOutlet var firstGroup: UILabel!
+    @IBOutlet var thridTitle: UILabel!
+    @IBOutlet var secondTitle: UILabel!
+    @IBOutlet var firstTitle: UILabel!
     @IBOutlet var thridButton: UIButton!
     @IBOutlet var secondButton: UIButton!
     @IBOutlet var firstButton: UIButton!
@@ -34,18 +40,42 @@ class ViewController: UIViewController {
 
     // MARK: - Public Methods
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showPlayerViewController" {
+            if let destinationVC = segue.destination as? PlayerViewController {
+                if let data = sender as? (UIImage?, String?, String?) {
+                    destinationVC.logoImage = data.0
+                    destinationVC.music = data.1
+                    destinationVC.group = data.2
+                }
+            }
+        }
+    }
+
     // MARK: - IBAction
 
     @IBAction func tapFirstButton(_ sender: Any) {
-        performSegue(withIdentifier: "showPlayerViewController", sender: nil)
+        performSegue(
+            withIdentifier: "showPlayerViewController",
+
+            sender: (firstMusicImageView.image, firstTitle.text, firstGroup.text)
+        )
     }
 
     @IBAction func tapSecondButton(_ sender: Any) {
-        performSegue(withIdentifier: "showPlayerViewController", sender: nil)
+        performSegue(
+            withIdentifier: "showPlayerViewController",
+
+            sender: (secondMusicImageView.image, secondTitle.text, secondGroup.text)
+        )
     }
 
     @IBAction func tapThridButton(_ sender: Any) {
-        performSegue(withIdentifier: "showPlayerViewController", sender: nil)
+        performSegue(
+            withIdentifier: "showPlayerViewController",
+
+            sender: (thridMusicImageView.image, thridTitle.text, thridGroup.text)
+        )
     }
 
     // MARK: - Private Methods
